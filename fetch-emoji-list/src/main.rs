5fc2851ab313c41e0a684ebc0fd6de0644c83bd5
Patrick Monaghan <0x5f.manpat@gmail.com>
2020-07-07 02:02:08 +0100

    filter out some unwanted codepoint combos for emoji; factor out http fetch stuff; add filter to hide emoji using tone modifiers; add visual feedback for invalid regex

--- fetch-emoji-list/src/main.rs
+++ fetch-emoji-list/src/main.rs
@@ -5,31 +5,22 @@ use std::io::{Write, Read};
 
 use serde_derive::Serialize;
 
-const UNICODE_VERSION: &str = "13.0";
 
 fn main() -> Result<(), Box<dyn Error>> {
-	let mut stream = TcpStream::connect("unicode.org:80")?;
-	write!(&mut stream, "GET /Public/emoji/{}/emoji-test.txt HTTP/1.1\r\n", UNICODE_VERSION)?;
-	write!(&mut stream, "Host: unicode.org\r\n")?;
-	write!(&mut stream, "\r\n")?;
+	let time = std::time::Instant::now();
 
-	let mut response = String::new();
-	stream.read_to_string(&mut response)?;
+	let emoji_test_data = fetch("unicode.org", "/Public/emoji/13.0/emoji-test.txt")?;
+	// let emoji_test_data = fetch("unicode.org", "/Public/emoji/13.0/emoji-sequences.txt"))?; // TODO
+	// let emoji_test_data = fetch("unicode.org", "/Public/emoji/13.0/emoji-zwj-sequences.txt"))?; // TODO
 
-	if response.find("200 OK").is_none() {
-		return Err(GenericError::new("Server didn't send the stuff"))
-	}
+	let info = parse_emoji_info(&emoji_test_data);
+	let data = serde_json::to_string_pretty(&info)?;
+	let mut f = File::create("emotes.json")?;
+	write!(&mut f, "{}", data)?;
 
-	if let Some(start) = response.find("\r\n\r\n") {
-		let info = parse_emoji_info(&response[start..]);
-		let data = serde_json::to_string_pretty(&info)?;
-		let mut f = File::create("emotes.json")?;
-		write!(&mut f, "{}", data)?;
-		Ok(())
+	println!("Done {}ms", time.elapsed().as_millis());
 
-	} else {
-		Err(GenericError::new("Malformed response"))
-	}
+	Ok(())
 }
 
 
@@ -45,13 +36,13 @@ struct EmoteEntry {
 fn parse_emoji_info(raw: &str) -> Vec<EmoteEntry> {
 	use regex::Regex;
 
+	println!("Parsing...");
+
 	let group_re = Regex::new(r"# group: ").unwrap();
 	let subgroup_re = Regex::new(r"# subgroup: ").unwrap();
-	let emoji_re = Regex::new(r"([\dA-F ]+)\s*;.*E\d+.\d\s([\w ]+)").unwrap();
+	let emoji_re = Regex::new(r"([\dA-F ]+)\s*; ([\w\-]+)\s*#.*E\d+.\d\s([\w ]+)").unwrap();
 
 	let mut entries = Vec::new();
-
-	let time = std::time::Instant::now();
 
 	for group_chunk in group_re.split(raw).skip(1) {
 		let name = group_chunk.lines()
@@ -68,12 +59,18 @@ fn parse_emoji_info(raw: &str) -> Vec<EmoteEntry> {
 			let subgroup_name = if let Some(n) = name { n.trim() } else { continue };
 
 			for entry in emoji_re.captures_iter(subgroup_chunk) {
+				let qualification = entry.get(2).unwrap().as_str();
+				match qualification {
+					"minimally-qualified" | "component" => { continue }
+					_ => {}
+				}
+
 				let text: String = entry.get(1).unwrap().as_str()
 					.split_whitespace()
 					.map(|s| u32::from_str_radix(s, 16).ok().and_then(std::char::from_u32).unwrap())
 					.collect();
 
-				let name = entry.get(2).unwrap().as_str().to_owned();
+				let name = entry.get(3).unwrap().as_str().to_owned();
 				
 				entries.push(EmoteEntry {
 					text,
@@ -85,9 +82,32 @@ fn parse_emoji_info(raw: &str) -> Vec<EmoteEntry> {
 		}
 	}
 
-	println!("get_emoji_info {}ms", time.elapsed().as_millis());
-
 	entries
+}
+
+
+
+fn fetch(host: &str, resource: &str) -> Result<String, Box<dyn Error>> {
+	println!("Fetching {}...", resource);
+
+	let mut stream = TcpStream::connect(format!("{}:80", host))?;
+	write!(&mut stream, "GET {} HTTP/1.1\r\n", resource)?;
+	write!(&mut stream, "Host: {}\r\n", host)?;
+	write!(&mut stream, "\r\n")?;
+
+	let mut response = String::new();
+	stream.read_to_string(&mut response)?;
+
+	if response.find("200 OK").is_none() {
+		return Err(GenericError::new("Server didn't send the stuff"))
+	}
+
+	if let Some(start) = response.find("\r\n\r\n") {
+		Ok(response[start..].to_owned())
+
+	} else {
+		Err(GenericError::new("Response missing body"))
+	}
 }
 
 
